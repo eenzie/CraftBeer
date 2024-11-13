926fec9c0f93766a1d5ca178ffb0f3088add7821
eenzie <100802909+eenzie@users.noreply.github.com>
2024-11-13 13:11:52 +0100

    Adds user comments (Contact Form) feature

---
+++ CraftBeer/CraftBeer.Web/ViewModels/ContactForm.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CraftBeer.Web.ViewModels;
+
+public class ContactForm
+{
+    [Required(ErrorMessage = "Name is a required field")]
+    public string Name { get; set; } = "";
+
+    [Required]
+    [EmailAddress(ErrorMessage = "not a valid email")]
+    public string Email { get; set; } = "";
+
+    [Required(ErrorMessage = "Subject is a required field")]
+    public string Subject { get; set; } = "";
+
+    [Required(ErrorMessage = "Message is a required field")]
+    public string Message { get; set; } = "";
+}
